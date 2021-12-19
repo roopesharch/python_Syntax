a3633cc5b1d449b1847479113d65b0efd01038e5
Roopesh S <42025266+roopesharch@users.noreply.github.com>
2021-12-19 10:19:22 +0530

    Create Access_email.py

---
+++ Access_email.py
@@ -0,0 +1,36 @@
+import smtplib
+import time
+import imaplib
+import email
+import traceback 
+
+
+FROM_EMAIL = "your email"
+FROM_PWD = "your_passowrd" 
+
+# this is for gmail,So change based on service provider name
+SMTP_SERVER = "imap.gmail.com" 
+
+
+
+mail = imaplib.IMAP4_SSL(SMTP_SERVER)
+mail.login(FROM_EMAIL,FROM_PWD)
+mail.select('inbox')
+
+data = mail.search(None, 'ALL')
+mail_ids = data[1]
+id_list = mail_ids[0].split()   
+first_email_id = int(id_list[0])
+latest_email_id = int(id_list[-1])
+otp=[]
+for i in range(latest_email_id,first_email_id, -1):
+    data = mail.fetch(str(i), '(RFC822)' )
+####print all the inbox mails
+    print(str(data),"\n++++++++++++========\n")
+#   get all the text in the field sign in:
+    a1=re.findall('sign in: \d+', str(data))
+
+    if len(a1)!=0:
+        for i in a1:
+            otp.append(str(i).replace("sign in: ",""))
+# print(opt)
